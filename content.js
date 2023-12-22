af645e81c51a80803144472162d6948336a2040d
floatsinkfc <floatsinkfc@gmail.com>
2023-12-21 20:32:07 -0600

    update

--- content.js
+++ content.js
@@ -3,92 +3,141 @@ var buttonsAndRowAdded = false;
 
 // Function to dynamically add "X" marks to graded rows
 function addDeleteButtons() {
-  if (!buttonsAndRowAdded) {
-    var gradedRows = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelectorAll("#grades_wrapper .graded_item_row");
+  var runTheRevamp = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelectorAll("#grades_wrapper .graded_item_row").length > 0;
+  if (runTheRevamp) {
+    if (!buttonsAndRowAdded) {
+      var gradedRows = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelectorAll("#grades_wrapper .graded_item_row");
 
-    gradedRows.forEach(function (row) {
-      var deleteButton = document.createElement("span");
-      deleteButton.className = "delete-row-button";
-      deleteButton.textContent = "X";
+      gradedRows.forEach(function (row) {
+        var deleteButton = document.createElement("span");
+        deleteButton.className = "delete-row-button";
+        deleteButton.setAttribute("style", "cursor: pointer; font-size: 15px;");
+        deleteButton.textContent = "X";
 
-      deleteButton.addEventListener("click", function () {
-        row.remove();
-        calculateAndDisplayTotal(); // Recalculate total after row removal
+        deleteButton.addEventListener("click", function () {
+          row.remove();
+          calculateAndDisplayTotal(); // Recalculate total after row removal
+        });
+
+        row.appendChild(deleteButton);
       });
-
-      row.appendChild(deleteButton);
-    });
-    
-    addTotalRow();
-    makeBox()
-    
+      
+      gradePage()
+      
+    }
   }
 }
 
-// Function to add a new row at the top for total grades, total points, and percentage
-function addTotalRow() {
-  var totalRow = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("div");
-  totalRow.className = "total-row row";
 
-  var descriptionBox = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("div");
-  descriptionBox.className = "cell gradable";
-  
-  var description = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("span");
-  description.textContent = "Class Total Grade (By Zi Jun Wang)"
 
-  var extlink = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("a");
-  extlink.textContent = "Extension Website"
-  extlink.setAttribute("href", "https://floatsink.me");
-  extlink.setAttribute("target", "_blank");
 
-  var totalGradeCell = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("div");
-  totalGradeCell.className = "cell grade";
-  totalGradeCell.textContent = "Total Grades: ";
-  
-
-  var totalPointsCell = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("div");
-  totalPointsCell.className = "cell pointsPossible";
-  totalPointsCell.textContent = "";
-
-  var percentageCell = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("div");
-  percentageCell.className = "cell percentage grade";
-  percentageCell.textContent = "Percentage: ";
-
-  totalRow.appendChild(descriptionBox);
-  descriptionBox.appendChild(description);
-  descriptionBox.appendChild(extlink);
-
-  totalRow.appendChild(totalGradeCell);
-  totalRow.appendChild(totalPointsCell);
-  totalRow.appendChild(percentageCell);
-
-  var gradesTable = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelector("#grades_wrapper");
-  gradesTable.insertBefore(totalRow, gradesTable.firstChild);
-}
-
-function makeBox() {
+function gradePage() {
+  //Flex
   var flexBox = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelector(".contentBox");
   flexBox.setAttribute("style", "display: grid; grid-template-columns: repeat(2, minmax(0, 1fr));");
+
+  //Remove MyGrade title make flex
   var myGradeRemove = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelector("#pageTitleDiv");
   myGradeRemove.remove()
 
+  //Get Class Name
   var className = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelector("#crumb_1");
   className = className.innerText;
 
-
+  //Change Container
   var containerOver = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelector("#containerdiv");
-  containerOver.setAttribute("style", "overflow-y: scroll; max-height:90vh; overflow-x: hidden;");
+  containerOver.setAttribute("style", "overflow-y: scroll; max-height:87vh; min-height:87vh; overflow-x: hidden;");
+
 
   var classData = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("div");
-  classData.setAttribute("style", "display: flex; justify-content: center; margin-top: 20px; background: white; padding: 20px; border: 2px solid #cdcdcd; max-height:90vh; ");
+  classData.setAttribute("style", "display: flex; margin-top: 20px; background: white; padding: 20px 10% 20px 10%; border: 2px solid #cdcdcd; max-height:87vh; flex-wrap: wrap; flex-direction: column; align-items: center; margin-right: 10px;");
+
   
-  var classDataTitle = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("h1");
+  //Class Box
+  var classDataTitle = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("div");
   classDataTitle.className = "uic-class-title";
-  classDataTitle.setAttribute("style", "font-size: 30px;");
-  classDataTitle.textContent = "Classes: " + className;
+  classDataTitle.setAttribute("style", "border-radius: 20px; border: 1px solid #000; display:flex; justify-content: center; align-items: center; width: 100%; height: 139px; flex-direction: column;");
+  
+
+  var classTitle = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("h1");
+  classTitle.textContent = "Classes: "
+  classTitle.setAttribute("style", "color: #000; text-align: center; font-size: 40px; margin: 0px;");
+
+  var classDataName = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("p");
+  classDataName.textContent = className
+  classDataName.setAttribute("style", "color: #000; text-align: center; font-size: 20px;");
+
+  classDataTitle.append(classTitle)
+  classDataTitle.append(classDataName)
+  
+
+  //Grade Section
+  var grading = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("div");
+  grading.setAttribute("style", "display:flex; justify-content: space-between; align-items: center; width: 100%; height: 150px; flex-direction: row;");
+  
+  //Total Grade
+  var totalGradeBox = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("div");
+  totalGradeBox.className = "total_grades";
+  totalGradeBox.setAttribute("style", "display:flex; justify-content: center; align-items: center; width: 48%; height: 130px; flex-direction: column ; border-radius: 20px; border: 1px solid #000;");
+
+  var totalGradeText = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("h1");
+  totalGradeText.textContent = "Total Points (Grades/Points)";
+  totalGradeText.setAttribute("style", "color: #000; text-align: center; font-size: 20px; margin: 0px;");
+
+
+  var totalGradeData = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("p");
+  totalGradeData.className = "uic-grade";
+  totalGradeData.textContent = "";
+  totalGradeData.setAttribute("style", "color: #000; text-align: center; font-size: 20px;");
+
+  totalGradeBox.append(totalGradeText);
+  totalGradeBox.append(totalGradeData);
+
+  //Total Grade
+  var percentageBox = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("div");
+  percentageBox.className = "total_percentage";
+  percentageBox.setAttribute("style", "display:flex; justify-content: center; align-items: center; width: 48%; height: 130px; flex-direction: column ; border-radius: 20px; border: 1px solid #000;");
+
+  var percentageText = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("h1");
+  percentageText.textContent = "Percentage";
+  percentageText.setAttribute("style", "color: #000; text-align: center; font-size: 20px; margin: 0px;");
+
+
+  var percentageData = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("p");
+  percentageData.className = "uic-percentage";
+  percentageData.textContent = "";
+  percentageData.setAttribute("style", "color: #000; text-align: center; font-size: 20px;");
+
+  percentageBox.append(percentageText);
+  percentageBox.append(percentageData);
+
+
+  grading.append(totalGradeBox);
+  grading.append(percentageBox);
+
+
+  //Misc
+  var miscContent = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("div");
+  miscContent.className = "miscContent";
+  miscContent.setAttribute("style", "border-radius: 20px; border: 1px solid #000; display:flex; width: 100%; height: 50vh; flex-direction: column;");
+
+
+  var searchBar = document.querySelector(".classic-learn-iframe").contentWindow.document.createElement("input");
+  searchBar.type = "text";
+  searchBar.setAttribute("style", "margin-top: 10px; margin-left: 10px; width: 95%; height: 53px; border-radius: 17px; border: 1px solid #000; font-size: 20px; ");
+  searchBar.placeholder = "Search for an assignment...";
+  searchBar.addEventListener("input", function (event) {
+    var searchTerm = event.target.value.toLowerCase();
+    filterAndDisplayResults(searchTerm);
+  });
+
+  
+  miscContent.append(searchBar)
+
 
   classData.append(classDataTitle);
-
+  classData.append(grading);
+  classData.appendChild(miscContent);
 
   
 
@@ -99,6 +148,22 @@ function makeBox() {
 
 
 }
+
+
+function filterAndDisplayResults(searchTerm) {
+  var assignments = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelectorAll(".sortable_item_row .gradable");
+
+  assignments.forEach(function (assignment) {
+    var courseName = assignment.textContent.toLowerCase();
+    var isVisible = courseName.includes(searchTerm);
+
+    var gradedItemRow = assignment.closest('.sortable_item_row');
+    if (gradedItemRow) {
+      gradedItemRow.style.display = isVisible ? "block" : "none";
+    }
+  });
+}
+
 
 
 
@@ -127,25 +192,15 @@ function calculateAndDisplayTotal() {
     });
   }
 
-  // Filter out rows with evaluation links
-  var rowsWithEvalLinks = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelectorAll(".sortable_item_row.calculatedRow.row.expanded.eval-links");
-  var excludedRows = Array.from(rowsWithEvalLinks);
-
-  // Extract numeric content from grade elements, excluding rows with eval-links
+  // Extract numeric content from grade elements
   var numericContentsGrade = extractNumericValues(
-    Array.from(gradeElements).filter(function (element) {
-      // Check if the element's parent row is in the excludedRows list
-      return !excludedRows.includes(element.closest('.sortable_item_row'));
-    }),
+    Array.from(gradeElements),
     false
   );
 
-  // Extract numeric content from pointsPossible elements, excluding rows with eval-links
+  // Extract numeric content from pointsPossible elements
   var numericContentsPointsPossible = extractNumericValues(
-    Array.from(pointsPossibleElements).filter(function (element) {
-      // Check if the element's parent row is in the excludedRows list
-      return !excludedRows.includes(element.closest('.sortable_item_row'));
-    }),
+    Array.from(pointsPossibleElements),
     true
   );
 
@@ -172,32 +227,34 @@ function calculateAndDisplayTotal() {
   console.log("Total Points Possible:", totalPointsPossible);
   console.log("Percentage:", percentage);
 
-  var totalGradeCell = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelector(".total-row .grade");
-  var totalPointsCell = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelector(".total-row .pointsPossible");
-  var percentageCell = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelector(".total-row .percentage");
+  var totalGradeCell = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelector(".total_grades .uic-grade");
+  var percentageCell = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelector(".total_percentage .uic-percentage");
 
   totalGradeCell.textContent = totalGrade.toFixed(2) + " / " + totalPointsPossible.toFixed(2);
   percentageCell.textContent = "Percentage: " + percentage.toFixed(2) + "%";
 }
 
+
+
+
+
+
 // Listen for messages from the popup
 browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === "checkID") {
+  if (request.action === "gradeRevamp") {
+    
     addDeleteButtons();
-
+    
     calculateAndDisplayTotal();
 
     sendResponse({});
-  } else if (request.action === "removeButtons") {
+  } 
+  else if (request.action === "removeButtons") {
     var deleteButtons = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelectorAll(".delete-row-button");
     deleteButtons.forEach(function (button) {
       button.remove();
     });
 
-    var totalRow = document.querySelector(".classic-learn-iframe").contentWindow.document.querySelector(".total-row");
-    if (totalRow) {
-      totalRow.remove();
-    }
 
     buttonsAndRowAdded = false;
 
@@ -205,7 +262,10 @@ browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     calculateAndDisplayTotal(); // Recalculate and update the top row
 
     sendResponse({});
-  } else {
-    sendResponse({});
+  } 
+  else if (request.action === "myGradeChange") {
+  }
+  else {
+
   }
 });
